0ca184da852121f6cd92390782c04043b0c97a7a
Param Chheda <paramkchheda@gmail.com>
2025-06-03 15:07:07 +0530

    implemented modulesclear

---
+++ 7. Modules/app.py
@@ -0,0 +1,7 @@
+# import converter
+# from converter import kg_to_lbs
+# kg_to_lbs(100)
+import utils
+numbers = [10, 3, 6, 12]
+max = utils.find_max(numbers)
+print(max)
---
+++ 7. Modules/converter.py
@@ -0,0 +1,6 @@
+def kg_to_lbs(x):
+    return x / 0.45
+
+
+def lbs_to_kg(x):
+    return x * 0.45
