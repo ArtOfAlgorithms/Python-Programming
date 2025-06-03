c51360c142dda6b13286ddf3120f4261ce774d4a
Param Chheda <paramkchheda@gmail.com>
2025-06-03 14:20:27 +0530

    Implemented inheritance, also renamed files

---
+++ 6. Inheritance.py
@@ -0,0 +1,17 @@
+class Mammal:
+    def walk(self):
+        print("walk")
+
+
+class Dog(Mammal):
+    def bark():
+        print("bark")
+
+
+class Cat(Mammal):
+    def meow():
+        print("meow")
+
+
+dog1 = Dog()
+dog1.walk()
